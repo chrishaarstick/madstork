80800eca16bef91b585e7a505a11844af1c65729
Chris Haarstick <chrishaarstick@gmail.com>
2018-02-19 16:11:53 -0500

    Updated Description file
    Added initial Portfolio Object function
    
    Added testthat folder

---
+++ R/Portfolio.R
@@ -0,0 +1,21 @@
+library(R6)
+
+# Portfolio Object
+Portfolio <- R6Class(classname = "Portfolio",
+
+                     public = list(
+                       name = NULL,
+                       cash = NA,
+                       holdings = NA,
+                       trades = NA,
+                       activity = NA
+                     ),
+
+                     initialize = function(name) {
+                       self$name <- name
+                       self$cash <- NA
+                       self$holdings <- NA
+                       self$trades <- NA
+                       self$activity <- NA
+                     }
+)
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(madstork)
+
+test_check("madstork")
