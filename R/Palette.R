f8f8db911b454fea69c9e70cf03fee4a716584d9
Chris Haarstick <chrishaarstick@gmail.com>
2018-03-24 15:05:43 -0400

    Adding Palette functions
    * changed name to update_market_*
    
    * added generic print method for portfolio

---
+++ R/Palette.R
@@ -0,0 +1,85 @@
+
+# Palette -----------------------------------------------------------------
+
+
+# madstork colors
+madstork_colors <- c(
+  `orange`     = "#f2671d",
+  `dark grey`  = "#8c8c8c",
+  `dark blue`  = "#00009a",
+  `light grey` = "#cccccc",
+  `light blue` = "#1da8f2"
+ )
+
+
+#' Function to extract madstork colors as hex codes
+#'
+#' @param ... Character names of madstork_colors
+#'
+madstork_cols <- function(...) {
+  cols <- c(...)
+
+  if (is.null(cols))
+    return (madstork_colors)
+
+  madstork_colors[cols]
+}
+
+madstork_palettes <- list(
+  `main`  = madstork_cols("orange", "dark grey", "dark blue", "light grey"),
+
+  `grey`  = madstork_cols("dark grey", "light grey")
+)
+
+
+#' Return function to interpolate a madstork color palette
+#'
+#' @param palette Character name of palette in madstork_palettes
+#' @param reverse Boolean indicating whether the palette should be reversed
+#' @param ... Additional arguments to pass to colorRampPalette()
+#'
+madstork_pal <- function(palette = "main", reverse = FALSE, ...) {
+  pal <- madstork_palettes[[palette]]
+
+  if (reverse) pal <- rev(pal)
+
+  colorRampPalette(pal, ...)
+}
+
+
+#' Color scale constructor for madstork colors
+#'
+#' @param palette Character name of palette in madstork_palettes
+#' @param discrete Boolean indicating whether color aesthetic is discrete or not
+#' @param reverse Boolean indicating whether the palette should be reversed
+#' @param ... Additional arguments passed to discrete_scale() or
+#'            scale_color_gradientn(), used respectively when discrete is TRUE or FALSE
+#'
+scale_color_madstork <- function(palette = "main", discrete = TRUE, reverse = FALSE, ...) {
+  pal <- madstork_pal(palette = palette, reverse = reverse)
+
+  if (discrete) {
+    discrete_scale("colour", paste0("madstork_", palette), palette = pal, ...)
+  } else {
+    scale_color_gradientn(colours = pal(256), ...)
+  }
+}
+
+
+#' Fill scale constructor for madstork colors
+#'
+#' @param palette Character name of palette in madstork_palettes
+#' @param discrete Boolean indicating whether color aesthetic is discrete or not
+#' @param reverse Boolean indicating whether the palette should be reversed
+#' @param ... Additional arguments passed to discrete_scale() or
+#'            scale_fill_gradientn(), used respectively when discrete is TRUE or FALSE
+#'
+scale_fill_madstork <- function(palette = "main", discrete = TRUE, reverse = FALSE, ...) {
+  pal <- madstork_pal(palette = palette, reverse = reverse)
+
+  if (discrete) {
+    discrete_scale("fill", paste0("madstork_", palette), palette = pal, ...)
+  } else {
+    scale_fill_gradientn(colours = pal(256), ...)
+  }
+}
--- R/Portfolio.R
+++ R/Portfolio.R
@@ -273,6 +273,39 @@ get_tax_liability <- function(pobj) {
 }
 
 
+#' Settle Portfolio Tax Liability
+#'
+#' Function to settle the tax liabilty. Option to make cash withdraw and add to
+#' portfolio activity
+#'
+#' @param pobj portfolio object
+#' @param date date of transaction. default is current date
+#' @param amount amount of tax settlement
+#' @param withdraw logical option to make a cash withdraw from portfolio
+#'
+#' @return updated portfolio object
+#' @export
+#'
+#' @examples
+#'portfolio("new_port", cash=0) %>%
+#'  make_deposit(amount = 2000) %>%
+#'  make_buy(Sys.Date()-1, symbol = "SPY", quantity = 10, price = 100) %>%
+#'  make_sell(id = 1, quantity = 5, price = 105) %>%
+#'  settle_tax_liability(amount = 7.5, withdraw = TRUE)
+settle_tax_liability <- function(pobj, date = Sys.Date(), amount, withdraw = FALSE){
+  stopifnot(class(pobj) == "portfolio")
+
+  if(withdraw){
+    pobj <- make_withdraw(pobj, date, amount, desc = "Tax Payment")
+  }
+  pobj$tax_liability <- pobj$tax_liability - amount
+
+
+  pobj
+}
+
+
+
 #' Get Portfolio Investment Income
 #'
 #' Returns realized income only. Does not estimate future income payments
@@ -307,9 +340,9 @@ get_income <- function(pobj) {
 
 
 
-#' Get Porfolio Holding's Market Value
+#' Updated Porfolio Holding's Market Value
 #'
-#' Gets current market price and annual dividends for portfolio holdings.
+#' Updateds current market price and annual dividends for portfolio holdings.
 #' Calculates market value, income and yield
 #'
 #' @param pobj
@@ -324,10 +357,10 @@ get_income <- function(pobj) {
 #'  make_deposit(amount = 5000) %>%
 #'  make_buy(Sys.Date()-365, symbol = "SPY", quantity = 10, price = 200) %>%
 #'  make_buy(Sys.Date()-365, symbol = "TLT", quantity = 25, price = 100)
-#' get_holdings_market_value(p1)
+#'  update_holdings_market_value(p1)
 #' }
 #'
-get_holdings_market_value <- function(pobj) {
+update_holdings_market_value <- function(pobj) {
   stopifnot(class(pobj) == "portfolio")
   holdings <- get_holdings(pobj)
   symbols <- holdings$symbol
@@ -396,13 +429,13 @@ get_holdings_market_value <- function(pobj) {
 #'  make_deposit(amount = 5000) %>%
 #'  make_buy(Sys.Date()-365, symbol = "SPY", quantity = 10, price = 200) %>%
 #'  make_buy(Sys.Date()-365, symbol = "TLT", quantity = 25, price = 100)
-#' update_holdings_market_value(p1)
+#'  update_market_value(p1)
 #' }
-update_market_value <- function(pobj){
+update_market_value <- function(pobj) {
   stopifnot(class(pobj) == "portfolio")
 
   holdings <- get_holdings(pobj)
-  holdings_market_value <- get_holdings_market_value(pobj)
+  holdings_market_value <- update_holdings_market_value(pobj)
 
   current_market_value <- data.frame(
     last_updated = Sys.time(),
@@ -410,11 +443,47 @@ update_market_value <- function(pobj){
     investments_value = sum(holdings_market_value$market_value),
     investments_annual_income = sum(holdings_market_value$annual_income),
     loans = as.numeric(0),
-    tax_liability = as.numeric(get_tax_liability(pobj))) %>%
+    tax_liability = as.numeric(get_tax_liability(pobj))
+  ) %>%
     dplyr::mutate(net_value = cash + investments_value - loans - tax_liability)
 
   pobj$holdings_market_value <- holdings_market_value
-  pobj$market_value <- rbind(pobj$market_value, current_market_value)
+  pobj$market_value <-
+    rbind(pobj$market_value, current_market_value)
 
   pobj
+}
+
+
+#'@rdname print
+print.portfolio <- function(pobj){
+  stopifnot(class(pobj) == "portfolio")
+
+  cat("Portfolio", pobj$name, "\n")
+  cat("---------------------------", "\n")
+
+  if(nrow(pobj$market_value)>0){
+    mv <- filter(pobj$market_value, last_updated == max(last_updated))
+    cat("Market Value:  ", "\n")
+    cat("* Net Value   ", scales::dollar(mv$net_value), "\n")
+    cat("* Investments ", scales::dollar(mv$investments_value), "\n")
+    cat("* Cash        ", scales::dollar(mv$cash), "\n")
+    cat("* Annual Income", scales::dollar(mv$investments_annual_income),"\n\n")
+  }
+
+  if(nrow(pobj$holdings_market_value) > 0){
+    cat("Top 5 Holdings by Market Value:", "\n")
+    pobj$holdings_market_value %>%
+      dplyr::top_n(5, market_value) %>%
+      dplyr::arrange(-market_value) %>%
+      dplyr::select(symbol, market_value, cost_basis, unrealized_gain) %>%
+      print()
+    cat("\n")
+  }
+
+  cat("Recent Activity:", '\n')
+  get_activity(pobj) %>%
+    dplyr::top_n(5, id) %>%
+    dplyr::arrange(-id) %>%
+    print()
 }
